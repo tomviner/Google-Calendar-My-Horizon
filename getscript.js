c345a6f17d35defe58aea0b59e0263b3d38239f5
Tom V <tom@viner.tv>
2011-08-23 21:06:57 +0200

    replace '\xc2\xa0' with normal spaces.

--- getscript.js
+++ getscript.js
@@ -1,23 +1,22 @@
 (function() {
   var URL_TO_FETCH = '//raw.github.com/tomviner/Google-Calendar-My-Horizon/master/myhorizon.js';
 
-  window.getScript = function (url, success){
+  window.getScript = function (url, success){
     var credit = "Based on http://pastie.org/462639";
-    var script = document.createElement('script');
-    script.src = url;
-    var head = document.getElementsByTagName('head')[0],
-        done = false;
-    script.onload = script.onreadystatechange = function(){
-      if ( !done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete') ){
-        done = true;
-        success();
-        script.onload = script.onreadystatechange = null;
-        head.removeChild(script);
-      }
-    };
-    head.appendChild(script);
-  };
+    var script = document.createElement('script');
+    script.src = url;
+    var head = document.getElementsByTagName('head')[0],
+        done = false;
+    script.onload = script.onreadystatechange = function(){
+      if ( !done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete') ){
+        done = true;
+        success();
+        script.onload = script.onreadystatechange = null;
+        head.removeChild(script);
+      }
+    };
+    head.appendChild(script);
+  };
 
-  getScript(URL_TO_FETCH, function(){});
+  getScript(URL_TO_FETCH, function(){});
 })();
-
